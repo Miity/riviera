79b619682a91286410a5c5d9bcb432db31254852
Miity <miityad@gmail.com>
2017-04-17 00:27:20 +0300

    _

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from firstsite import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
